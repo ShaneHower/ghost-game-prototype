1b560061814eb350f84033f689c578ed8e66ba88
Shane Hower <shane.hower@gmail.com>
2022-04-23 18:32:48 -0400

    Implemented a basic start menu (#3)

---
+++ StartMenu.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject menuOptions;
+    public GameObject gameCoordinatorObject;
+
+    private Dictionary<string, MenuButton> buttons = new Dictionary<string, MenuButton>();
+    private GameCoordinator gameCoordinator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        getButtons();
+        gameCoordinator = gameCoordinatorObject.GetComponent<GameCoordinator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (buttons["start"].isActive)
+        {
+            activateStart();
+        }
+        else if (buttons["options"].isActive)
+        {
+            activateOption();
+        }
+        else if (buttons["exit"].isActive)
+        {
+            activateExit();
+        }
+    }
+    
+
+    void getButtons()
+    {
+        foreach(Transform child in menuOptions.transform)
+        {
+            if(child.tag == "button")
+            {
+                MenuButton button = child.GetComponent<MenuButton>();
+                buttons[button.name] = button;
+            }
+        }
+    }
+
+
+    void activateStart()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            gameCoordinator.activateGame = true;
+        }
+    }
+
+    void activateOption()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            return;
+        }
+    }
+
+    void activateExit()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            return;
+        }
+    }
+}
