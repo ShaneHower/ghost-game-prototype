1c29cff10fb5c0cccfe87fde0eb64778de991089
ShaneHower <shane.hower@gmail.com>
2022-04-23 18:29:23 -0400

    Implemented a basic start menu

--- GameCoordinator.cs
+++ GameCoordinator.cs
@@ -1,18 +1,64 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameCoordinator : MonoBehaviour
 {
+    public GameObject startMenu;
+    public GameObject game;
+    public int countDown;
+
+    public bool activateGame;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        activateStartMenu();
+        countDown = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        monitorStartMenuInput();
+
     }
+
+    void activateStartMenu()
+    {
+        startMenu.SetActive(true);
+        game.SetActive(false);
+    }
+
+    void monitorStartMenuInput()
+    {
+
+        if(activateGame)
+        {
+            startGame();
+        }
+        else
+        {
+            countDown = 2;
+            activateStartMenu();
+        }
+    }
+
+    void startGame()
+    {
+        startMenu.SetActive(false);
+        game.SetActive(true);
+        StartCoroutine(countDownLoop());
+    }
+
+
+    IEnumerator countDownLoop()
+    {
+        int i = 0;
+        if (i <= countDown)
+        {
+            countDown -= i;
+        }
+       yield return new WaitForSeconds(0.5f);
+    }
+
 }
---
+++ MenuButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    public string name;
+    public bool isActive;
+
+    private Image highlight;
+
+    private void Start()
+    {
+        highlight = GetComponent<Image>();
+        highlight.enabled = false;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            isActive = true;
+            highlight.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            isActive = false;
+            highlight.enabled = false;
+        }
+    }
+}
